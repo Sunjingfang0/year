df3d959f13ccbc116f4f78f2bce5e895a4a1f40b
Sunjingfang0 <751146824@qq.com>
2018-01-31 22:56:48 +0800

    作业

--- src/J12/N1214.java
+++ src/J12/N1214.java
@@ -1,13 +1,24 @@
 package J12;
 
 import java.io.File;
+
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
 
 public class N1214 {
-	public static void main(String[] args) throws IOException {
-	
-		File file=new File("E:\\sd.java");
-		//System.out.println(file.exists());
-		
+	public static void main(String[] args) throws IOException  {
+		File file = new File("E:/sd.java");
+		Reader reader=new FileReader(file);
+		char[] c=new char[(int) file.length()];
+		reader.read(c);
+//		String d=new String(c);
+		File file1=new File("F:/sd.java");
+		Writer writer=new FileWriter(file1);
+		writer.write(c);
+		writer.close();
+		reader.close();
 	}
 }
