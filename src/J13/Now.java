0b53077f7757dcfe96018c35fbe7b5fc3d65b74b
Sunjingfang0 <751146824@qq.com>
2018-01-31 16:51:51 +0800

    first

---
+++ src/J13/Now.java
@@ -0,0 +1,24 @@
+package J13;
+
+import java.util.Scanner;
+
+public class Now {
+	public static void main(String[] args) {
+	//Scanner scanner=new Scanner(System.in);
+	//String tr=scanner.nextLine();
+	//String  srt=scanner.next();
+	//String  srt1=scanner.next();
+	//String aaa=scanner.nextLine();
+	//System.out.println("输入的内容为"+srt);
+	//String  tr="123";
+	//String tr1="5";
+	//System.out.println(Integer.parseInt(tr)+Integer.parseInt(tr1));
+		//Thread t1=new Thread1();
+		Runnable t2=new Thread2();
+		//t1.run();
+		//t2.run();
+		//t1.start();
+		t2.run();
+		
+	}
+}
